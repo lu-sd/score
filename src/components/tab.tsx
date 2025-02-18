b9ced8f6b8d4b0e5bfe675f17348d059baae6f17
lu-sd <luhou.me@gmail.com>
2025-02-18 12:11:49 -0800

    improved css no shift

--- src/components/tab.tsx
+++ src/components/tab.tsx
@@ -9,21 +9,39 @@ type item =
 type props = { items: item[] }
 
 function Tab({ items }: props) {
-  const [isShow, setIsShow] = useState("html")
+  const [isShow, setIsShow] = useState<string>("html");
+
   return (
-    <div className="flex gap-3">
-      {items.map((item) => (
-        <div key={item.value}>
-          <button onClick={() => setIsShow(item.value)}>{item.label}</button>
-          {/* <div className="max-w-xs" hidden={item.value !== isShow}>{item.panel}</div> */}
-          {item.value === isShow && <p>{item.panel}</p>}
-        </div>
-      ))
-      }
-    </div >
+    <div className="w-full max-w-md mx-auto">
+      {/* Tab Buttons */}
+      <div className="flex border-b border-gray-300">
+        {items.map((item) => (
+          <button
+            key={item.value}
+            onClick={() => setIsShow(item.value)}
+            className={`px-4 py-2  ${isShow === item.value
+              ? "border-b-2 border-blue-500 text-blue-600 font-semibold"
+              : "text-gray-500 hover:text-blue-600"
+              }`}
+          >
+            {item.label}
+          </button>
+        ))}
+      </div>
+
+      {/* Tab Content */}
+      <div className="mt-4">
+        {items.map((item) => (
+          <div
+            key={item.value}
+            hidden={isShow != item.value}>
+            {item.panel}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
-
 export default function Tabs() {
   return <Tab
     items={[
