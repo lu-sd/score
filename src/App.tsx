af9ed5a957bb281d04591351ee8a204c5734237c
lu-sd <luhou.me@gmail.com>
2025-02-20 14:46:42 -0800

    dataTable

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,12 @@ import TemConvert from "./components/temConvert";
 import Mortgage from "./components/mortgate";
 import Tabs from "./components/tab";
 import Clock from "./components/clock"
+import DataTable from "./components/dataTable";
 function App() {
   return (
     <div className="flex flex-col gap-4 justify-center min-w-full items-center">
       <div className="text-3xl mt-8">hello moon</div>
+      <DataTable />
       <Clock />
       <Tabs />
       <Form />
