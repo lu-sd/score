c239f1a2f596d954e75a0702e88807fc0c90c149
lu-sd <luhou.me@gmail.com>
2025-02-23 11:28:15 -0800

    usestate scope is not right

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,12 @@ import Tabs from "./components/tab";
 import Clock from "./components/clock"
 import DataTable from "./components/dataTable";
 import Dice from "./components/dice.tsx"
+import File from "./components/file"
 function App() {
   return (
     <div className="flex flex-col gap-4 justify-center min-w-full items-center">
       <div className="text-3xl mt-8">hello moon</div>
+      <File />
       <Dice />
       <DataTable />
       <Clock />
---
+++ src/components/file.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+
+type fileData = { id: number; name: string; children?: fileData[] }
+const data = [
+  {
+    id: 1,
+    name: 'README.md',
+  },
+  {
+    id: 2,
+    name: 'Documents',
+    children: [
+      {
+        id: 3,
+        name: 'Word.doc',
+      },
+      {
+        id: 4,
+        name: 'Powerpoint.ppt',
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: 'Downloads',
+    children: [
+      {
+        id: 6,
+        name: 'unnamed.txt',
+      },
+      {
+        id: 7,
+        name: 'Misc',
+        children: [
+          {
+            id: 8,
+            name: 'foo.txt',
+          },
+          {
+            id: 9,
+            name: 'bar.txt',
+          },
+        ],
+      },
+    ],
+  },
+];
+export default function File() {
+
+  return (
+    <div>
+      <div className="text-blue-400">This is File Explorer </div>
+      <RecursiveList data={data} />
+    </div>
+  )
+}
+
+function RecursiveList({ data }: { data: fileData[] }) {
+  const [expanded, setExpanded] = useState(false)
+  if (!data || !data.length) {
+    return null
+  }
+  return (
+    <ul>
+      {data.map((item) => {
+        const isDire = Boolean(item.children)
+        return (<li key={item.id}>
+          <button onClick={() => {
+            if (!isDire) {
+              return
+            }
+            setExpanded(!expanded)
+          }}><span>{item.name}</span>{isDire && <> {expanded ? '-' : "+"}</>}</button>
+          {item.children && <RecursiveList data={item.children} />}
+        </li>)
+      })}
+    </ul>
+  )
+}
