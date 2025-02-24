282154e60f424ee1413809b749863eec23fd9d7a
lu-sd <luhou.me@gmail.com>
2025-02-24 10:51:31 -0800

    add sort for dir and nondir

--- src/components/file.tsx
+++ src/components/file.tsx
@@ -59,9 +59,14 @@ function RecursiveList({ data }: { data: fileData[] }) {
   if (!data || !data.length) {
     return null
   }
+  const dir = data.filter((file) => file.children)
+  dir.sort((a, b) => a.name.localeCompare(b.name))
+  const nonDir = data.filter((file) => !file.children)
+  nonDir.sort((a, b) => a.name.localeCompare(b.name))
+  const items = [...dir, ...nonDir]
   return (
     <ul className="pl-4">
-      {data.map((item) =>
+      {items.map((item) =>
         < FileObject key={item.id} item={item} />)}
     </ul>
   )
@@ -78,7 +83,8 @@ function FileObject({ item }: { item: fileData }) {
         }
         setExpanded(!expanded)
       }}><span>{item.name}</span>{isDire && <> {expanded ? '-' : "+"}</>}</button>
-    {item.children && expanded && <RecursiveList data={item.children} />}
+    {/* This recursion allows the component to display nested levels of items without knowing in advance how deep the structure goes. */}
+    {item.children && item.children.length > 0 && expanded && <RecursiveList data={item.children} />}
   </li>)
 
 }
