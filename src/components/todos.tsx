4d73481dc882313c5c4019fd044576409d763773
lu-sd <luhou.me@gmail.com>
2025-03-16 12:21:39 -0700

    use form intead of input

--- src/components/todos.tsx
+++ src/components/todos.tsx
@@ -8,25 +8,41 @@ const INITIAL_TASKS = [
 ];
 export default function Todos() {
   const [todo, setTodo] = useState("")
-  const [tasks, settasks] = useState(INITIAL_TASKS)
-  function handleSubmit() {
+  const [tasks, setTasks] = useState(INITIAL_TASKS)
+  function handleSubmit(event: React.FormEvent) {
+    // Listen to onSubmit events so that it works for both "Enter" key and
+    // click of the submit <button>.
+    event.preventDefault();
+    // Trim the field and don't add to the list if it's empty.
+    if (todo.trim() === '') {
+      return;
+    }
+    // Trim the value before adding it to the tasks.
+    setTasks([
+      ...tasks,
+      { id: id++, label: todo.trim() },
+    ]);
     const newTasks = [...tasks, { id: id++, label: todo }]
-    settasks(newTasks)
+    setTasks(newTasks)
     setTodo("")
   }
   function handleDelete(id: number) {
     const newTasks = tasks.filter((task) => task.id !== id)
-    settasks(newTasks)
+    setTasks(newTasks)
   }
   return (
     <div className="flex flex-col gap-3">
       <h1 className="text-center">Todo List</h1>
-      <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
-      <button className=" border border-blue-500" onClick={handleSubmit}>Submit</button>
-      {tasks.map(({ id, label }) => <li key={id}>
-        <span>{label}</span>
-        <button className="border border-red-200" onClick={() => handleDelete(id)}>Delete</button>
-      </li>)}
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder="Add your task" />
+        <button className=" border border-blue-500">Submit</button>
+      </form>
+      <ul>
+        {tasks.map(({ id, label }) => <li key={id}>
+          {label}
+          <button className="ml-2 border border-red-200" onClick={() => handleDelete(id)}>Delete</button>
+        </li>)}
+      </ul>
     </div>
   )
 }
