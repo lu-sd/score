a8c7e535f15141e643ccea6d0608003b570e4a7f
lu-sd <luhou.me@gmail.com>
2025-03-17 16:28:43 -0700

    add style

--- src/components/traficLight.tsx
+++ src/components/traficLight.tsx
@@ -30,7 +30,7 @@ export default function TrafficLight() {
     return () => { clearTimeout(timerId) }
   }, [currentColor])
 
-  return (<div>
+  return (<div className="flex gap-4 bg-black rounded-l p-2 ">
     {Object.keys(config).map((color) => {
       const key = color as keyof typeof config
       return <Light key={color} backgroundColor={color === currentColor ? config[key].backgroundColor : ""} />
@@ -40,8 +40,7 @@ export default function TrafficLight() {
 
 function Light({ backgroundColor }: { backgroundColor: string }) {
 
-  return (<div>
-    {backgroundColor}
-  </div>)
+  return (<div className="bg-gray-400 h-3 w-3 rounded-full" style={{ backgroundColor }}>
+  </div >)
 
 }
